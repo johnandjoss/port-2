f628b2b588f40bdee0c5b0e13f4e7354315c4798
jocelynalsdorf <jfrodsla@gmail.com>
2015-07-02 08:15:13 -0700

    passing integration specs for adding a new project to a skill and any errors when adding a new project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,30 @@
 class ProjectsController < ApplicationController
 
+	def show
+	    @skill = Skill.find(params[:skill_id])
+	    @project = Project.find(params[:id])
+	end
+
+	def new
+	    @skill = Skill.find(params[:skill_id])
+	    @project = Project.new
+	end
+
+	def create
+	  @skill = Skill.find(params[:skill_id])
+	  @project = Project.new(project_params)
+	  if @project.save
+	    flash[:notice] = "Project added!!"
+	    redirect_to skill_path(@project.skill_id)
+	  else
+	    render :new
+	  end
+	end
 
 
+  private
+  def project_params
+    params.require(:project).permit(:title, :content, :link, :skill_id)
+  end
 
 end
---
+++ spec/features/add_new_project_pages_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "the add a project process" do
+  it "adds a new project" do
+    test_skill = Skill.create(:name => "Section One", :description => "this")
+    visit skill_path(test_skill)
+    click_on 'Add a Project'
+    fill_in 'Title', :with => 'Ruby'
+    fill_in 'Content', :with => 'Blah Blah'
+    fill_in 'Link', :with => "onetwo"
+    click_on 'Create Project'
+    expect(page).to have_content 'Section One'
+
+  end
+
+  it "gives an error when no data is entered into form fields" do
+    test_skill = Skill.create(:name => "Section One", :description => "this")
+    visit skill_path(test_skill)
+    click_on 'Add a Project'
+    fill_in 'Title', :with => ''
+    fill_in 'Content', :with => ''
+    fill_in 'Link', :with => ''
+    click_on 'Create Project'
+    expect(page).to have_content 'errors'
+  end
+end
