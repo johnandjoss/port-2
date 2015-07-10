56975961cf09fa5cae428a864232876f5c671fd9
jocelynalsdorf <jfrodsla@gmail.com>
2015-07-10 10:06:14 -0700

    add CRUD for comment which will be changed to admin only

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,15 @@
 class CommentsController < ApplicationController
 	before_action :authenticate_user!
 
+	def show
+		@comment = Comment.find(params[:id])
+		@post = Post.find(params[:post_id])
+	end
 
+	def edit
+		@post = Post.find(params[:post_id])
+		@comment = Comment.find(params[:id])
+	end
 
 
 	def create
@@ -14,7 +22,28 @@ class CommentsController < ApplicationController
 		else
 			render :new
 		end
-end
+	end
 
+	def update
+			@post = Post.find(params[:post_id])
+			@comment = Comment.find(params[:id])
+		if @comment.update(comment_params)
+			redirect_to @post
+		else
+			render :error
+		end
+	end
+
+	def destroy
+		@comment = Comment.find(params[:id])
+		@comment.destroy	
+		redirect_to root_path
+	end
+
+private
+
+	def comment_params
+		params.require(:comment).permit(:content)
+	end
 
 end
