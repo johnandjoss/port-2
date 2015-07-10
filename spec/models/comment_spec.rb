a9ea9beb3fd19e27aeb7e8676482d13e711bb3bc
jocelynalsdorf <jfrodsla@gmail.com>
2015-07-10 10:41:57 -0700

    add simple cov; rspec passing for 26 exam with 94.8% coverage

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+describe Comment do
+  it { should validate_presence_of :content }
+  it { should belong_to :user }
+  it { should belong_to :post }
 end
+
+
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Post do
+  it { should validate_presence_of :title}
+  it { should validate_presence_of :body }
+  it { should have_many :comments }
+  it { should belong_to :user }
 end
+
+
+
